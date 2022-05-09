1fc8b9b6302e1583e9f0d21b017d787094b35097
Nnaji Chimuanya <nnajichimuanya50@gmail.com>
2022-05-09 20:15:10 +0400

    Gotten my UI right

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { BsChevronDoubleRight } from "react-icons/bs";
 
 
 const url = "https://course-api.com/react-tabs-project"
@@ -24,8 +25,40 @@ function App() {
     return <h1>Loading...</h1>
   }
 
-  return <div>
-    Jobs
+  const { company, dates, duties, title } = jobs[value]
+  return <div className='main'>
+    <div className='container'>
+      <div className='titleDiv row'>
+        Experience
+      </div>
+
+      <div className='job row'>
+        <div className='col-md-3 btn-container'> 
+          {
+            jobs.map((job, index)=> {
+              return (
+                <button key={job.id} onClick={() => setValue(index)} className={`jobButton ${index === value && 'activeButton'}`}> {job.company} </button>
+              )
+            })
+          }
+        </div>
+
+        <div className='col-md-9 details'>
+          <h2>{title}</h2>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {
+            duties.map((duty, index) => {
+              return (
+                <div className='duty'>
+                  <BsChevronDoubleRight className='arrow' /> <p key={index}>{duty}</p>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+  </div>
   </div>
 
 
